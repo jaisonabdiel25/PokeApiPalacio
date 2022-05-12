354cb660d737d7807b3be304955c890dedb99026
Jaison Palacio <jeisonpalacio0710@gmail.com>
2022-05-12 14:04:19 -0500

    Prmer add

---
+++ utils.js
@@ -0,0 +1,32 @@
+(() => {
+    const Utils = {
+      settings: {
+        backendBaseUrl: "https://pokeapi.co/api/v2",
+      },
+      getFormattedBackendUrl: ({ query, searchType }) => {
+        return `${Utils.settings.backendBaseUrl}/${searchType}/${query}`;
+      },
+      getPokemon: ({ query, searchType = "pokemon" }) => {
+        return Utils.fetch({
+          url: Utils.getFormattedBackendUrl({ query, searchType }),
+          searchType,
+        });
+      },
+      getEvolution:  (url)=>{
+        return Utils.fetch({url, searchType: ''})
+      },
+      fetch: async ({ url, searchType }) => {
+        try {
+          const rawResponse = await fetch(url);
+          if (rawResponse.status !== 200) {   
+            throw new Error(`${searchType} not found`);
+          }
+          return rawResponse.json();
+        } catch (error) {
+          
+          throw error;
+        }
+      },
+    };
+    document.Utils = Utils;
+  })();
